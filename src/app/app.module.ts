7ee9058d1e83d5639dcdf687a635574798589526
Nueng Pasit <onetwotee1@gmail.com>
2022-06-16 13:56:49 +0700

    edit position component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DistributionListComponent } from './distribution/distribution-list/distribution-list.component';
-import { DistributionEditComponent } from './distribution/distribution-list/distribution-edit/distribution-edit.component';
+import { DistributionListComponent } from './distribution-list/distribution-list.component';
+import { DistributionEditComponent } from './distribution-list/distribution-edit/distribution-edit.component';
 import { AquaticFoodComponent } from './aquatic-food/aquatic-food.component';
 import { AquaticFoodListComponent } from './aquatic-food/aquatic-food-list/aquatic-food-list.component';
-import { AquaticFoodItemComponent } from './aquatic-food/aquatic-food-item/aquatic-food-item.component';
-import { AquaticFoodDetailComponent } from './aquatic-food/aquatic-food-detail/aquatic-food-detail.component';
+import { AquaticFoodItemComponent } from './aquatic-food/aquatic-food-list/aquatic-food-item/aquatic-food-item.component';
+import { AquaticFoodDetailComponent } from './aquatic-food/aquatic-food-list/aquatic-food-detail/aquatic-food-detail.component';
 
 @NgModule({
   declarations: [
